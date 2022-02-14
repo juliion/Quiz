7b4b35541f7ef2bb3d235a50747634f00435cac6
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-02-14 18:51:28 +0200

    Add new user registration method

--- Quiz/Users.cs
+++ Quiz/Users.cs
@@ -9,5 +9,15 @@ namespace Quiz
     public class Users
     {
         private List<User> _users;
+        public bool SignUp(string login, string password, DateTime birthday)
+        {
+            foreach (User user in _users)
+            {
+                if(login == user.Login)
+                    return false;
+            }
+            _users.Add(new User(login, password, birthday));
+            return true;
+        }
     }
 }
