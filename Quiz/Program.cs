fe1ae96b4cfdad7c72388d71ccc112296ed55479
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-02-15 19:07:09 +0200

    Add class Menu

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quiz.Services;
 
 namespace Quiz
 {
@@ -10,6 +11,27 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
+            do
+            {
+                Menu.Display();
+                switch (Menu.GetChoice())
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("  Неверный символ!");
+                        break;
+                }
+
+
+            } while (Menu.AllowContinue());
+            Console.WriteLine("\n\nПрограмма завершена!");
         }
     }
 }
