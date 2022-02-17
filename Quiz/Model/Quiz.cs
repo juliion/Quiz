ca1d2d663b94ab04b30029ea7083d0aeed0e97be
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-02-17 12:41:25 +0200

    Add class for represent quiz

---
+++ Quiz/Model/Quiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Model
+{
+    public class Quiz
+    {
+        public QuizType Type { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Quiz/Model/QuizType.cs
@@ -0,0 +1,7 @@
+﻿namespace Quiz.Model
+{
+    public enum QuizType
+    {
+
+    }
+}
