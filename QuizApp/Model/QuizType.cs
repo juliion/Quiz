4e60d9f213606f704eab3e4675c466b6c2974149
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-02-18 17:44:43 +0200

    Add 3 files with quiz

--- QuizApp/Model/QuizType.cs
+++ QuizApp/Model/QuizType.cs
@@ -2,6 +2,7 @@
 {
     public enum QuizType
     {
-
+        History,
+        Physics
     }
 }
--- QuizApp/Services/QuizManager.cs
+++ QuizApp/Services/QuizManager.cs
@@ -10,10 +10,34 @@ namespace QuizApp.Services
     public class QuizManager
     {
         private List<Quiz> _quizzes;
-
+        private string[] _filenames;
         public QuizManager()
         {
             _quizzes = new List<Quiz>();
+            _filenames = new string[] { @"..\..\Data\Quiz1.json", @"..\..\Data\Quiz2.json", @"..\..\Data\Quiz3.json" };
+            foreach (var filename in _filenames)
+            {
+                _quizzes.Add(DataManager.LoadQuiz(filename));
+            }
+        }
+        public int[] StartQuiz(int ind)
+        {
+            List <Question> questions = _quizzes[ind].Questions;
+            int[] userAnswers = new int[questions.Count];
+            int indUA = 0;
+            foreach (var question in questions)
+            {
+                Console.Clear();
+                Console.WriteLine(question.Text);
+                Console.WriteLine();
+                foreach (var answer in question.Answers)
+                {
+                    Console.WriteLine(answer.Text);
+                }
+                 userAnswers[indUA] = Int32.Parse(Console.ReadLine());
+                indUA++;
+            }
+            return userAnswers;
         }
     }
 }
