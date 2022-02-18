c25b821803124a384b2622fcc6f61ec2f8cc17d5
Julia Ionova <71667162+juliion@users.noreply.github.com>
2022-02-18 14:55:59 +0200

    Add class for manage Quize

---
+++ QuizApp/Services/QuizManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizApp.Model;
+
+namespace QuizApp.Services
+{
+    public class QuizManager
+    {
+        private List<Quiz> _quizzes;
+
+        public QuizManager()
+        {
+            _quizzes = new List<Quiz>();
+        }
+    }
+}
