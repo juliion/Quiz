a58b6da16d267e817403d8960adb4119a8ff47c9
juliion <juliaionova111@gmail.com>
2022-03-05 14:26:25 +0200

    Add sign in and sign up in QuizCreator.

--- QuizCreator/Program.cs
+++ QuizCreator/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuizCreator.Services;
+using QuizApp.Services;
+using Menu = QuizCreator.Services.Menu;
 
 namespace QuizCreator
 {
@@ -10,6 +13,47 @@ namespace QuizCreator
     {
         static void Main(string[] args)
         {
+            UserManager um = new UserManager();
+            bool exit = false;
+            int choice;
+            do
+            {
+                Menu.DisplayRegMenu();
+                choice = Menu.GetChoice();
+                switch (choice)
+                {
+                    case 1:
+                        um.DisplaySignIn();
+                        break;
+                    case 2:
+                        um.DisplaySignUp();
+                        break;
+                }
+            } while (choice < 1 || choice > 2);
+            do
+            {
+                Menu.DisplayMainMenu();
+                switch (Menu.GetChoice())
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("  Неверный символ!");
+                        break;
+                }
+                if (exit)
+                    break;
+            } while (Menu.AllowContinue());
+            Console.WriteLine("\n\nПрограмма завершена!");
         }
     }
 }
