793fb6b75ff2ff87ceff1e812cc2483a0d09ff0f
juliion <juliaionova111@gmail.com>
2022-03-07 15:49:15 +0200

    Add a class Manager to manage the creation/deletion/editing of quizzes.

---
+++ QuizCreator/Services/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuizModel;
+using QuizApp.Services;
+
+namespace QuizCreator.Services
+{
+    public class Manager
+    {
+        private List<Quiz> _createdQuizzes;
+        private string[] _filenames;
+
+        public Manager()
+        {
+            _createdQuizzes = new List<Quiz>();
+            _filenames = Directory.GetFiles(@"../../../QuizApp/Data/Quizzes");
+            foreach (var filename in _filenames)
+            {
+                _createdQuizzes.Add(DataManager.LoadQuiz(filename));
+            }
+        }
+
+        public void AddQuiz(Quiz newQuiz)
+        {
+            _createdQuizzes.Add(newQuiz);
+            string filenameQuiz = @"../../../QuizApp/Data/Quizzes/Quiz" + _createdQuizzes.Count;
+            _filenames[_filenames.Length - 1] = filenameQuiz;
+            DataManager.SaveQuiz(filenameQuiz, newQuiz);
+        }
+    }
+}
