bd03422b49b8d4a34389473c588acb3f52f8e3e0
juliion <juliaionova111@gmail.com>
2022-05-16 13:54:47 +0300

    Replace data manager with reader and writer classes.

---
+++ QuizApp/Services/JSONWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace QuizApp.Services
+{
+    public class JSONWriter<T> : Writer<T> where T : class
+    {
+        private string _fileName;
+
+        public JSONWriter(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public override void Write(T data)
+        {
+            string jsonString = JsonSerializer.Serialize<T>(data, new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            });
+            File.WriteAllText(_fileName, jsonString);
+        }
+    }
+}
